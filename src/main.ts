1df0c4a1b29b395877ffcc4524b93e047955c3ef
wcs <1103409364@qq.com>
2022-01-09 01:31:00 +0800

    config: element-ui 去除全局引入
    Signed-off-by: wcs <1103409364@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,8 @@ import { createApp, h } from "vue-demi";
 import "windi.css";
 import router from "@/router";
 import store from "./store";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
 import i18n from "@/lang/index";
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite"; //loadEnv
 import { createVuePlugin } from "vite-plugin-vue2";
 import legacy from "@vitejs/plugin-legacy";
 import WindiCSS from "vite-plugin-windicss";
