7062f4024128a0f968d4b49466a586e9694adcb9
wcs <1103409364@qq.com>
2022-01-09 21:35:38 +0800

    config: 按需引入 element-ui 全局组件样式
    Signed-off-by: wcs <1103409364@qq.com>

---
+++ src/config/element-ui.config.ts
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import { Message, Notification, MessageBox, Loading } from "element-ui";
+import "element-ui/lib/theme-chalk/message.css";
+import "element-ui/lib/theme-chalk/notification.css";
+import "element-ui/lib/theme-chalk/loading.css";
+import "element-ui/lib/theme-chalk/message-box.css";
+Vue.prototype.$message = Message;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$loading = Loading;
+Vue.prototype.$ELEMENT = { size: "medium" };
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import { createApp, h } from "vue-demi";
-import { Message, Notification, MessageBox, Loading } from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
 import "windi.css";
+import "@/config/element-ui.config";
 import "@/config/permission.config";
 import router from "@/router";
 import store from "./store";
 import i18n from "@/lang/index";
-
-Vue.prototype.$message = Message;
-Vue.prototype.$notify = Notification;
-Vue.prototype.$confirm = MessageBox.confirm;
-Vue.prototype.$loading = Loading;
-Vue.prototype.$ELEMENT = { size: "medium" };
 
 Vue.config.productionTip = false;
 if (import.meta.env.MODE !== "production") {
