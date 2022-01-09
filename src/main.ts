b2fe2f7c0ae45fd18b26ccc042da6b48595df5ed
wcs <1103409364@qq.com>
2022-01-09 16:12:36 +0800

    config: element-ui 补充配置
    Signed-off-by: wcs <1103409364@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import { createApp, h } from "vue-demi";
+import { Message, Notification, MessageBox, Loading } from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 import "windi.css";
 import router from "@/router";
 import store from "./store";
 import i18n from "@/lang/index";
 
+Vue.prototype.$message = Message;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$loading = Loading;
+Vue.prototype.$ELEMENT = { size: "medium" };
+
 Vue.config.productionTip = false;
-Vue.config.devtools = true;
+if (import.meta.env.MODE !== "production") {
+  Vue.config.devtools = true;
+}
 
 const app = createApp({
   router,
