a5fa910c57f8fd7c6882e70fed918ceeca9bcd2f
wcs <1103409364@qq.com>
2022-01-31 18:41:06 +0800

    fix: jsx element ui组件配置属性报重载错误
    Signed-off-by: wcs <1103409364@qq.com>

--- src/types/shims-tsx.d.ts
+++ src/types/shims-tsx.d.ts
@@ -1,18 +1,21 @@
 import Vue, { VNode } from "vue";
 
+import Vue, { VNode } from "vue";
+import { ComponentRenderProxy } from "@vue/composition-api";
+
 declare global {
   namespace JSX {
-    // tslint:disable no-empty-interface
     interface Element extends VNode {}
-    // tslint:disable no-empty-interface
-    interface ElementClass extends Vue {}
+    interface ElementClass extends ComponentRenderProxy {}
+    interface ElementAttributesProperty {
+      $props: Record<string, unknown>; // specify the property name to use
+    }
     interface IntrinsicElements {
       [elem: string]: any;
     }
-  }
-
-  interface ImportMeta {
-    env: Record<any, string>;
-    globEager<T = unknown>(globPath: string): Record<string, T>;
+    interface ImportMeta {
+      env: Record<any, string>;
+      globEager<T = unknown>(globPath: string): Record<string, T>;
+    }
   }
 }
